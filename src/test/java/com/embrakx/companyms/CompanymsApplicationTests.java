d02bc28c7b6ca73ed44c3ba4a36f0e16e5cf7afe
Harshal <farkadeharshal14@gmail.com>
2025-05-08 11:19:15 +0530

    first commit

---
+++ src/test/java/com/embrakx/companyms/CompanymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embrakx.companyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
