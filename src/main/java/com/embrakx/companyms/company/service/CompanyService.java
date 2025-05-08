9d1ca769de05e5f40d2736c23eafb8742fa4d9bd
Harshal <farkadeharshal14@gmail.com>
2025-05-08 14:28:44 +0530

    Developement is done

---
+++ src/main/java/com/embrakx/companyms/company/service/CompanyService.java
@@ -0,0 +1,20 @@
+package com.embrakx.companyms.company.service;
+
+import com.embrakx.companyms.company.entity.Company;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CompanyService {
+
+    List<Company>  getAllCompany();
+
+    Company updateCompany(Long id,Company company);
+
+    Company createCompany(Company company);
+
+    boolean deleteCompany(Long id);
+
+    Company getCompanyById(Long id);
+}
